c3ac43b0836cd1fde7913a2ea9cb25e02f5bb5e8
Faizmohammad <42071980+Faizgeeky@users.noreply.github.com>
2021-01-28 13:09:09 +0530

    Update pg7.java

--- pg7.java
+++ pg7.java
@@ -1,9 +1,12 @@
-class pg7{
+class Main{
 
  public static void main(String[] args) {
 
    Vehicle obj1 = new Vehicle();
+   
+   
    obj1.carname();
+   obj1.model();
   }
 
 
@@ -11,15 +14,18 @@ class pg7{
 
 abstract class Car {
 
-
+ 
   public abstract void carname();
-
+  public void model() {
+    System.out.println("2021");
+  }
 
 }
 
 class Vehicle extends Car {
+    
   public void carname() {
-
+    
     System.out.println("Fortuner");
   }
 }
