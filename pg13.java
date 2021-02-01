77d55443f7756a797ee187f3a2f95ff683cbc842
Faizmohammad <42071980+Faizgeeky@users.noreply.github.com>
2021-02-01 08:30:58 +0530

    Update pg13.java

--- pg13.java
+++ pg13.java
@@ -1,12 +1,23 @@
-class pg13{
-
-public static void main(String[] args) {
-
-  try {
-    System.out.println(1/0);
-  }catch (Exception e) {
-    System.out.println("Divisible by Zero");
-  }
-
-}
+public class Main
+{  
+  public static void main(String[] args) 
+  {  
+    try
+    {    
+      int a[]=new int[10];    
+      System.out.println(a[0]);  
+    }    
+    catch(ArithmeticException e)  
+    {  
+      System.out.println("Arithmetic Exception --> "+e);  
+    }    
+    catch(ArrayIndexOutOfBoundsException e)  
+    {  
+      System.out.println("ArrayIndexOutOfBounds Exception --> "+e);  
+    }    
+    catch(Exception e)  
+    {  
+      System.out.println(e);  
+    }                 
+  }  
 }
