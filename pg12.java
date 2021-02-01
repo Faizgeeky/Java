d0dd457081d2b6620cf2a5deebc1292e7519190b
Faizmohammad <42071980+Faizgeeky@users.noreply.github.com>
2021-02-01 08:29:04 +0530

    Update pg12.java

--- pg12.java
+++ pg12.java
@@ -1,25 +1,40 @@
-
-public class pg12{
-
-	public static void main(String [] args) {
-
-		b obj1 = new b();
-		System.out.println(obj1.ab());
-	}
-
+class Main{
+    
+    public static void main (String[] args) {
+        
+        Bike obj1 = new Bike(80000,2019);
+        Bike obj2 = new Bike(100000,2021);
+        
+        System.out.println(obj1.calculatePrice());
+        System.out.println(obj2.calculatePrice());
+        
+        
+    }
 }
 
-class a{
-
-	int ab() {
-		return 10;
-	}
-
+abstract class Shape {
+    abstract String calculatePrice();
 }
-class b extends a{
 
-	int ab() {
-		return 5;
-	}
+class Bike extends Shape {
+    private final double price;
+    private final double model;
 
+    Bike(double price, double model) {
+        this.price = price;
+        this.model = model;
+        
+    
+    }
+
+    @Override
+    String calculatePrice() {
+        String p = String.valueOf(price);  
+        String m = String.valueOf(model);  
+        return String.format("Bike with a model of  %s has price %s",m,p);   
+        // return "s";
+                
+    }
+
+  
 }
