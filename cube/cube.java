24c7cbe10f7a8ae6954c596e14a9e9f8d461c65e
Faizmohammad <42071980+Faizgeeky@users.noreply.github.com>
2021-02-01 13:40:21 +0530

    Create cube.java

---
+++ cube/cube.java
@@ -0,0 +1,22 @@
+package cube;
+
+public class cube {
+	
+	public  static int Cube(int a)
+	{
+		return(a*a*a);
+	}
+	
+	public static float Cube(float a)
+	{
+		return(a*a*a);
+	}
+	
+	public static double Cube(double a)
+	{
+		return(a*a*a);
+	}
+	
+		
+
+}
